baecf17abb5eace5fc932e0ca329cd3c550b8e37
Dmytro Zhelieznyi <Dmytro_Zhelieznyi@emap.com>
2020-03-25 18:02:58 +0200

    Initial commit

---
+++ src/main/java/com/qozz/worldwidehotelsystem/WorldwideHotelSystemApplication.java
@@ -0,0 +1,13 @@
+package com.qozz.worldwidehotelsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldwideHotelSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldwideHotelSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qozz/worldwidehotelsystem/WorldwideHotelSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qozz.worldwidehotelsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldwideHotelSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
